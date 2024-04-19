d9a9795ae588e186c2f463c012154617d552616b
fcaussa <68918444+fcaussa@users.noreply.github.com>
2024-04-19 11:08:57 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,55 +7,12 @@ if [ $UID != 0 ]; then
     exit 1
 fi
 
-#check we have all the required variables!
-[ -z ${1+z} ] && echo "APN is set to $1" || echo "APN Not defined" && exit 1;
-[ -z ${2+z} ] && echo "Serial Port is set to $2" || echo "Serial Port Not defined" && exit 1;
-[ -z ${3+z} ] && echo "Baudrate is set to $3" || echo "Modem Baudrate Not defined" && exit 1;
-[ -z ${4+z} ] && echo "Pin5 is set to $4" || echo "Pin5 Not defined" && exit 1;
-[ -z ${5+z} ] && echo "Pin6 is set to $5" || echo "Pin6 Not defined" && exit 1;
-[ -z ${6+z} ] && echo "Pin13 is set to $6" || echo "Pin13 Not defined" && exit 1;
-[ -z ${7+z} ] && echo "Pin18 is set to $7" || echo "Pin18 Not defined" && exit 1;
-[ -z ${8+z} ] && echo "Pin19 is set to $8" || echo "Pin19 Not defined" && exit 1;
-[ -z ${9+z} ] && echo "Pin21 is set to $9" || echo "Pin21 Not defined" && exit 1;
-[ -z ${10+z} ] && echo "Pin26 is set to ${10}" || echo "Pin26 Not defined" && exit 1;
+#echo "install ppp"
+#apt-get install ppp
 
-
-echo "\n\n Intalling Rak2013 LTE Module"
-
-#$1 => APN 
-#$2 => Serial Port: /dev/ttyS0
-#$3 => baudrate: 115200
-
-#$4: pin5
-#$5: pin6
-#$6: pin13
-#$7: pin18
-#$8: pin19
-#$9: pin21
-#$10: pin26
-
-echo "Updating repositories..."
-
-apt update
-
-echo "Install ppp"
-
-apt-get install ppp
-
-#echo "creating directories"
+echo "creating directories"
 #mkdir -p /etc/chatscripts
 #mkdir -p /etc/ppp/peers
-
-#Delete folder and content if already exists!
-rm -rf /usr/local/rak2013 2>/dev/null
-
-rm /etc/chatscripts/quectel-chat-connect 2>/dev/null
-
-rm /etc/chatscripts/quectel-chat-disconnect 2>/dev/null
-
-rm /etc/ppp/peers/gprs 2>/dev/null
-
-mkdir -p /usr/local/rak
 
 echo "creating script file : /etc/chatscripts/quectel-chat-connect"
 echo "
@@ -87,10 +44,9 @@ SAY \"\nGoodbay\n\"" > /etc/chatscripts/quectel-chat-disconnect
 
 echo "creating script file : /etc/ppp/peers/gprs"
 echo "
-#/dev/ttyS0 115200
-$2 $3
+/dev/ttyS0 115200
 # The chat script, customize your APN in this file
-connect 'chat -s -v -f /etc/chatscripts/quectel-chat-connect -T $1'
+connect 'chat -s -v -f /etc/chatscripts/quectel-chat-connect -T internet.movil'
 # The close script
 disconnect 'chat -s -v -f /etc/chatscripts/quectel-chat-disconnect'
 # Hide password in debug messages
@@ -121,109 +77,7 @@ remotename 3gppp
 ipparam 3gppp
 ipcp-max-failure 30
 # Ask the peer for up to 2 DNS server addresses
-
 usepeerdns" > /etc/ppp/peers/gprs
 
+echo "\n\nUse \"sudo pppd call gprs\" command and Surf"
 
-pin5=$4
-pin6=$5
-pin13=$6
-pin18=$7
-pin19=$8
-pin21=$9
-pin26=${10}
-
-
-echo "
-#!/bin/sh
-
-#Controller is gpio512
-#inputs must be 512 + pin number
-# 5     -> 517
-# 6     -> 518
-# 13    -> 525
-# 19    -> 531
-# 21    -> 533
-# 26    -> 538
-
-# 18 (PWR)-> 530
-
-cleanup_gpio()
-{
-    if [ -d /sys/class/gpio/gpio\$1 ]
-    then
-        echo \"\$1\" > /sys/class/gpio/unexport; sleep 0.2
-    fi
-}
-
-cd /sys/class/gpio/
-
-if [ ! -d /sys/class/gpio/gpio$pin5 ]; then
-    echo 517 > export
-fi
-
-if [ ! -d /sys/class/gpio/gpio$pin6 ]; then
-    echo 518 > export
-fi
-
-if [ ! -d /sys/class/gpio/gpio$pin13 ]; then
-    echo 525 > export
-fi
-
-if [ ! -d /sys/class/gpio/gpio$pin19 ]; then
-    echo 531 > export
-fi
-
-if [ ! -d /sys/class/gpio/gpio$pin21 ]; then
-    echo 533 > export
-fi
-
-if [ ! -d /sys/class/gpio/gpio$pin26 ]; then
-    echo 538 > export
-fi
-
-
-echo out > gpio$pin5/direction
-echo out > gpio$pin6/direction
-echo out > gpio$pin13/direction
-echo out > gpio$pin19/direction
-echo in > gpio$pin21/direction
-echo out > gpio$pin26/direction
-
-echo 0 > gpio$pin5/value
-echo 0 > gpio$pin6/value
-echo 0 > gpio$pin13/value
-echo 0 > gpio$pin21/value
-echo 0 > gpio$pin26/value
-
-
-#
-cd /sys/class/gpio/
-
-if [ ! -d /sys/class/gpio/gpio$pin18 ]; then
-    echo \"$pin18\" > /sys/class/gpio/export
-fi
-echo \"out\" > /sys/class/gpio/gpio$pin18/direction
-echo 0 > /sys/class/gpio/gpio$pin18/value
-sleep 0.2
-echo 1 > /sys/class/gpio/gpio$pin18/value
-sleep 0.2
-echo 0 > /sys/class/gpio/gpio$pin18/value
-
-sleep 20
-
-sudo pppd call gprs &
-
-sleep 10
-
-route add default ppp0
-
-" > /usr/local/rak2013/activate_lte
-
-
-
-echo "*********************************************************"
-echo "*  The RAK2013 LTE Module is successfully installed!   *"
-echo "*********************************************************"
-
-echo "\n\n Please reboot board!"
